506b0b0c99c7e132b3a550ac31d7e980c9df6f47
anushaprem29 <anushaprem29@missionrnd.com>
2016-03-17 15:39:04 +0530

    Submit Commit

--- src/012sllSort.cpp
+++ src/012sllSort.cpp
@@ -21,6 +21,52 @@ struct node {
 	struct node *next;
 };
 
+//Insert values into list.
+void push(struct node * head,int value){
+	struct node *newNode, *iter;
+
+	iter = head;
+	newNode = (struct node *)malloc(sizeof(struct node));
+	newNode->next = NULL;
+	newNode->data = value;
+	if (head == NULL)
+		head = newNode;
+	else{
+		while (iter->next != NULL){
+			iter = iter->next;
+		}
+		iter->next = newNode;
+	}
+}
+
+//Function to sort the linked list into 0,1 and 2
+void sort012(struct node *head){
+	struct node *iter = head;
+	int value[3] = { 0, 0, 0 }, count = 0;
+	if (iter == NULL)	return;
+	while (iter != NULL){
+		value[iter->data]++;
+		iter = iter->next;
+	}
+	if (head != NULL){
+		while (count < 3){
+			if (value[count]){
+				head->data = count;
+				head->next = NULL;
+				value[count]--;
+				break;
+			}
+			count++;
+		}
+	}
+	//head = NULL;
+	count = 0;
+	while (count<3){
+		while (value[count]--)
+			push(head,count);
+		count++;
+	}
+}
 void sll_012_sort(struct node *head){
-	
+	sort012(head);
 }
--- src/numberToLinkedList.cpp
+++ src/numberToLinkedList.cpp
@@ -20,5 +20,20 @@ struct node {
 };
 
 struct node * numberToLinkedList(int N) {
-	return NULL;
+	struct node *head = (struct node *)malloc(sizeof(struct node));
+	if (N < 0) N *= -1;
+	if (N == 0){
+		head->num = N;
+		head->next = NULL;
+		return head;
+	}
+	head = NULL;
+	while (N > 0){
+		struct node *newNode = (struct node *)malloc(sizeof(struct node));
+		newNode->num = N % 10;
+		N = N / 10;
+		newNode->next = head;
+		head = newNode;
+	}
+	return head;
 }
